4dd3e2bf539ed03df637b4a38eea29ad69d37882
MonkeyZ-04 <89679927+MonkeyZ-04@users.noreply.github.com>
2025-07-25 15:22:18 +0700

    Update

--- mroi_app/src/index.jsx
+++ mroi_app/src/index.jsx
@@ -3,20 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App.jsx';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
 import '@ant-design/v5-patch-for-react-19';
 
+// ตรวจสอบว่าเป็น Production mode หรือไม่ เพื่อกำหนด basename ให้ถูกต้อง
+const basename = import.meta.env.PROD ? '/mroi' : '/';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//--------- this code will render two time just in develop mode  not in production mode --------
-// root.render( 
-//   <React.StrictMode>
-//     <BrowserRouter> 
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
+
 root.render(
-  <BrowserRouter> 
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
